452ddfaa7edd16056f52d2230a33eb79b0c48900
Om Saraiya <omsaraiya1@gmail.com>
2026-07-20 00:18:45 +0530

    feature: mastered JS variables and handled constant assingnment errors

---
+++ script.js
@@ -0,0 +1,6 @@
+const developerName = "Om Saraiya";
+let currentGoal = "Junior Frontend Developer";
+
+console.log("Welcome to Day 2",developerName);
+console.log("Target Locked:",currentGoal);
+//developerName ="Rahul";
