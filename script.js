a129db08e9919fbe8d2a6e3aa20a4e05bf8f3d22
Om Saraiya <omsaraiya1@gmail.com>
2026-07-20 17:36:43 +0530

    feature: mastered tamplete literals and if-else loogic

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 const developerName = "Om Saraiya";
 let currentGoal = "Junior Frontend Developer";
 
-console.log("Welcome to Day 2",developerName);
-console.log("Target Locked:",currentGoal);
+console.log("Welcome to Day 2", developerName);
+console.log("Target Locked:", currentGoal);
 //developerName ="Rahul";
+
+let studentName = "Om Saraiya";
+let daysComplete = 3;
+let isFocused = true;
+
+/*function showProgress(name ,days){
+    console.log("Developer: " + name);
+    console.log("Status: " + days + "days completed. Keep going!");
+}
+
+showProgress(studentName, daysComplete);
+showProgress("Rahul", 10);*/
+
+function checkStatus(name, days, focus) {
+    if (focus === true) {
+        console.log(`Great job ${name}! You have perfectly completed ${days} days.`);
+    } else {
+        console.log(`${name}, you need to remove distractions and focus more.`);
+    }
+}
+
+
+checkStatus(studentName, daysComplete, isFocused);
+
+checkStatus("rahul", 10, false);
